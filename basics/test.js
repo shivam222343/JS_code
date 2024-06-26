1b08412a567dfdae2389c478b41bdace879f621f
shivam222343 <164057678+shivam222343@users.noreply.github.com>
2024-06-26 15:13:56 +0000

    config file

---
+++ basics/test.js
@@ -0,0 +1,21 @@
+//message = console.log("hello world");
+//
+//let name = "ram";
+//console.log(name);
+//console.log(typeof name);
+//
+//let newName = Number("ram");
+//console.log(newName);
+//console.log(typeof newName);
+
+let password = 3245;
+console.log(typeof password);
+
+let newPassword = String(3245);
+console.log(typeof newPassword);
+
+let loggedIn = true;
+console.log(typeof loggedIn);
+
+let newLoggedIn = Boolean(true);
+console.log(typeof newLoggedIn);
