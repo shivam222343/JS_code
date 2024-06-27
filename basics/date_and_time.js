743af17145932f2aea9a8f1051715df2cf0b7d08
shivam222343 <164057678+shivam222343@users.noreply.github.com>
2024-06-27 08:40:30 +0000

    date

---
+++ basics/date_and_time.js
@@ -0,0 +1,11 @@
+//let myCreatedDate = new Date(2024, 9, 19, 12, 9, 10, 15);
+//console.log(myCreatedDate.toString());
+//
+////for Indian Timeline
+let myIndianDate = new Date(19-10-2005);
+//console.log(myIndianDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(myTimeStamp.getTime());
+
