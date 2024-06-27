717a62814d3c291f86330e3e0a51633e72d71d81
shivam222343 <164057678+shivam222343@users.noreply.github.com>
2024-06-27 07:23:51 +0000

    config

--- basics/test.js
+++ basics/test.js
@@ -7,15 +7,38 @@
 //let newName = Number("ram");
 //console.log(newName);
 //console.log(typeof newName);
+//
+//let password = 3245;
+//console.log(typeof password);
+//
+//let newPassword = String(3245);
+//console.log(typeof newPassword);
+//
+//let loggedIn = true;
+//console.log(typeof loggedIn);
+//
+//let newLoggedIn = Boolean(true);
+//console.log(typeof newLoggedIn);
+//
+//let rame = {
+//    name:"Nikita",
+//    sirname:"Dombe",
+//    PhoneNumber:7709533591,
+//}
+//let e = "i am e";
+//
+//console.log(typeof rame);
+//
+//let ram = function(e){
+//    console.log("helloworld");
+//    console.log(e);
+//};
+//
+//ram();
 
-let password = 3245;
-console.log(typeof password);
+//Stings
 
-let newPassword = String(3245);
-console.log(typeof newPassword);
+let name = "ram"
+let count = 500
 
-let loggedIn = true;
-console.log(typeof loggedIn);
-
-let newLoggedIn = Boolean(true);
-console.log(typeof newLoggedIn);
+console.log(`my name is ${name} and my count is ${count}.`);
